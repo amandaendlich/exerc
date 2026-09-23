6f77baa960db4c2fb07758ed5069b1480c7e31c5
amandaendlich <amanda.esilva2@aluno.edu.es.gov.br>
2026-09-23 12:08:58 -0300

    aula3

--- script.js
+++ script.js
@@ -1,2 +1,81 @@
-document. addEventListener('Keydown', jump);
-const
+const mario = document.querySelector('.mario');
+
+const pipe = document.querySelector('.pipe');
+
+
+// FUNCAO PARA PULAR
+
+const jump = () => {
+
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump');
+
+    }, 500);
+
+                   }  ;
+
+
+// VERIFICACO DA COLISAO
+
+const loop = setInterval () => {
+
+    const pipePosition = pipe.offsetLeft;
+
+    const marioPosition = +window
+        .getComputedStyle(mario)
+        .bottom
+        .replace('px', '');
+
+
+    // COLISAO
+
+    if (
+        pipePosition <= 120 &&
+        pipePosition > 0 &&
+        marioPosition < 80
+    ) {
+
+        // PARA O CANO
+
+        pipe.style.animation = 'none';
+
+        pipe.style.left = `${pipePosition}px`;
+
+
+        // PARA O MARIO
+
+        mario.style.animation = 'none';
+
+        mario.style.bottom = `${marioPosition}px`;
+
+
+        // TROCA A IMAGEM
+
+        mario.src = './images/game-over.png';
+
+
+        // DIMINUI O MARIO
+
+        mario.style.width = '75px';
+
+
+        // POSICIONA O MARIO
+
+        mario.style.marginLeft = '50px';
+
+
+        // PARA O LOOP
+
+        clearInterval(loop);
+
+    }
+
+}, 10);
+
+
+// DETECTA UMA TECLA
+
+document.addEventListener('keydown', jump);
